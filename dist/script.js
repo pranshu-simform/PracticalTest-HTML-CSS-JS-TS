f7ef51b07b9175e926506dd6247330a4a13aa1f7
Pranshu Patel <pranshu.patel@simformsolutions.com>
2025-04-30 12:23:56 +0530

    Add(feature/logic): create static gameArray and 4x4 gameGrid

--- dist/script.js
+++ dist/script.js
@@ -1,1 +1,40 @@
 "use strict";
+class MemoryGame {
+    constructor() {
+        this.gameArray = new Array(16).fill('');
+        this.generateGameArray();
+        this.initEventlistner();
+    }
+    initEventlistner() {
+        var _a;
+        (_a = document.querySelector('.gamecontainer')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', (event) => {
+            if (event.target.tagName === 'BUTTON') {
+                const btnElement = event.target;
+                btnElement.textContent = btnElement.value;
+                btnElement.disabled = true;
+            }
+        });
+    }
+    generateGameArray() {
+        let len = this.gameArray.length;
+        for (let i = 0; i < len / 2; i++) {
+            this.gameArray[i] = i.toString();
+        }
+        for (let i = len / 2; i < len; i++) {
+            this.gameArray[i] = (i - len / 2).toString();
+        }
+        console.log(this.gameArray);
+        this.generateGrid();
+    }
+    generateGrid() {
+        const gamecontainer = document.querySelector('.gamecontainer');
+        for (let i = 0; i < this.gameArray.length; i++) {
+            const btnElement = document.createElement('button');
+            btnElement.value = this.gameArray[i];
+            gamecontainer.appendChild(btnElement);
+        }
+    }
+}
+document.addEventListener('DOMContentLoaded', (event) => {
+    new MemoryGame();
+});
--- scripts/script.ts
+++ scripts/script.ts
@@ -1,5 +1,46 @@
 class MemoryGame{
+    gameArray: string[];
+    currentPlayer: string;
 
+    constructor(){
+        this.gameArray=new Array(16).fill('');
+        this.generateGameArray();
+        this.initEventlistner();
+    }
+
+    initEventlistner(){
+        document.querySelector('.gamecontainer')?.addEventListener('click',(event: Event)=>{
+            if((event.target as HTMLElement).tagName==='BUTTON'){
+                const btnElement=event.target as HTMLButtonElement;
+                btnElement.textContent=btnElement.value;
+                btnElement.disabled=true;
+            }
+        })
+    }
+
+    generateGameArray(){
+        let len=this.gameArray.length;
+
+        for(let i=0;i<len/2;i++){
+            this.gameArray[i]=i.toString();
+        }
+
+        for(let i=len/2;i<len;i++){
+            this.gameArray[i]=(i-len/2).toString();
+        }
+
+        console.log(this.gameArray);
+        this.generateGrid();
+    }
+
+    generateGrid(){
+        const gamecontainer=document.querySelector('.gamecontainer');
+        for(let i=0;i<this.gameArray.length;i++){
+            const btnElement=document.createElement('button');
+            btnElement.value=this.gameArray[i];
+            gamecontainer!.appendChild(btnElement);
+        }
+    }
 }
 
 document.addEventListener('DOMContentLoaded',(event: Event)=>{
