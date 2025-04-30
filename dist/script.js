936883b38900540211094b21554da7793a3805fd
Pranshu Patel <pranshu.patel@simformsolutions.com>
2025-04-30 14:21:17 +0530

    Add(feature/logic): implemented reset game logic

--- dist/script.js
+++ dist/script.js
@@ -10,12 +10,15 @@ class MemoryGame {
         this.playerScore = [0, 0];
     }
     initEventlistner() {
-        var _a;
+        var _a, _b;
         (_a = document.querySelector('.gamecontainer')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', (event) => {
             if (event.target.tagName === 'BUTTON') {
                 const btnElement = event.target;
                 this.handleButtonclick(btnElement);
             }
+        });
+        (_b = document.querySelector('.resetgame')) === null || _b === void 0 ? void 0 : _b.addEventListener('click', (event) => {
+            this.resetGame();
         });
     }
     generateGameArray() {
@@ -26,11 +29,22 @@ class MemoryGame {
         for (let i = len / 2; i < len; i++) {
             this.gameArray[i] = (i - len / 2).toString();
         }
+        // this.suffleArray();
         console.log(this.gameArray);
         this.generateGrid();
     }
+    suffleArray() {
+        let len = this.gameArray.length;
+        for (let i = len - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            let temp = this.gameArray[i];
+            this.gameArray[i] = this.gameArray[j];
+            this.gameArray[j] = temp;
+        }
+    }
     generateGrid() {
         const gamecontainer = document.querySelector('.gamecontainer');
+        gamecontainer.innerHTML = '';
         for (let i = 0; i < this.gameArray.length; i++) {
             const btnElement = document.createElement('button');
             btnElement.value = this.gameArray[i];
@@ -56,16 +70,20 @@ class MemoryGame {
                 this.playerScore[this.currentPlayer] = this.playerScore[this.currentPlayer] + 1;
                 const currPlayerScore = document.getElementById(`scoreplayer${this.currentPlayer + 1}`);
                 currPlayerScore.textContent = `Score: ${this.playerScore[this.currentPlayer]}`;
+                this.firstCard = null;
+                this.secondCard = null;
             }
             else {
                 this.togglePlayer();
-                this.firstCard.textContent = '';
-                this.firstCard.disabled = false;
-                this.secondCard.textContent = '';
-                this.secondCard.disabled = false;
+                setTimeout(() => {
+                    this.firstCard.textContent = '';
+                    this.firstCard.disabled = false;
+                    this.secondCard.textContent = '';
+                    this.secondCard.disabled = false;
+                    this.firstCard = null;
+                    this.secondCard = null;
+                }, 500);
             }
-            this.firstCard = null;
-            this.secondCard = null;
         }
     }
     togglePlayer() {
@@ -74,6 +92,16 @@ class MemoryGame {
         this.currentPlayer = this.currentPlayer === 0 ? 1 : 0;
         const nextPlayer = document.getElementById(`player${this.currentPlayer + 1}`);
         nextPlayer.classList.add('playerActive');
+    }
+    resetGame() {
+        this.generateGameArray();
+        this.firstCard = null;
+        this.secondCard = null;
+        this.playerScore = [0, 0];
+        const scorePlayer1 = document.getElementById('scoreplayer1');
+        scorePlayer1.textContent = `Score: ${0}`;
+        const scorePlayer2 = document.getElementById('scoreplayer2');
+        scorePlayer2.textContent = `Score: ${0}`;
     }
 }
 document.addEventListener('DOMContentLoaded', (event) => {
--- scripts/script.ts
+++ scripts/script.ts
@@ -22,6 +22,10 @@ class MemoryGame{
                 this.handleButtonclick(btnElement);
             }
         })
+
+        document.querySelector('.resetgame')?.addEventListener('click',(event: Event)=>{
+            this.resetGame();
+        })
     }
 
     generateGameArray(){
@@ -35,12 +39,26 @@ class MemoryGame{
             this.gameArray[i]=(i-len/2).toString();
         }
 
+        // this.suffleArray();
+
         console.log(this.gameArray);
         this.generateGrid();
     }
 
+    suffleArray(){
+        let len=this.gameArray.length;
+
+        for(let i=len-1;i>0;i--){
+            const j=Math.floor(Math.random()*(i+1));
+            let temp=this.gameArray[i];
+            this.gameArray[i]=this.gameArray[j];
+            this.gameArray[j]=temp;
+        }
+    }
+
     generateGrid(){
         const gamecontainer=document.querySelector('.gamecontainer');
+        gamecontainer!.innerHTML='';
         for(let i=0;i<this.gameArray.length;i++){
             const btnElement=document.createElement('button');
             btnElement.value=this.gameArray[i];
@@ -67,18 +85,24 @@ class MemoryGame{
             if(this.firstCard.value===this.secondCard.value){
                 this.playerScore[this.currentPlayer]=this.playerScore[this.currentPlayer]+1;
                 const currPlayerScore=document.getElementById(`scoreplayer${this.currentPlayer+1}`);
-                currPlayerScore!.textContent=`Score: ${this.playerScore[this.currentPlayer]}`
+                currPlayerScore!.textContent=`Score: ${this.playerScore[this.currentPlayer]}`;
+
+                this.firstCard=null;
+                this.secondCard=null;
             }
             else{
                 this.togglePlayer();
-                this.firstCard.textContent='';
-                this.firstCard.disabled=false;
-                this.secondCard.textContent='';
-                this.secondCard.disabled=false;
+                setTimeout(()=>{
+                    this.firstCard!.textContent='';
+                    this.firstCard!.disabled=false;
+                    this.secondCard!.textContent='';
+                    this.secondCard!.disabled=false;
+
+                    this.firstCard=null;
+                    this.secondCard=null;
+                },500)
             }
 
-            this.firstCard=null;
-            this.secondCard=null;
         }
     }
 
@@ -90,6 +114,18 @@ class MemoryGame{
 
         const nextPlayer=document.getElementById(`player${this.currentPlayer+1}`);
         nextPlayer!.classList.add('playerActive');
+    }
+
+    resetGame(){
+        this.generateGameArray();
+        this.firstCard=null;
+        this.secondCard=null;
+        this.playerScore=[0,0];
+        const scorePlayer1=document.getElementById('scoreplayer1');
+        scorePlayer1!.textContent=`Score: ${0}`;
+
+        const scorePlayer2=document.getElementById('scoreplayer2');
+        scorePlayer2!.textContent=`Score: ${0}`;
     }
 }
 
