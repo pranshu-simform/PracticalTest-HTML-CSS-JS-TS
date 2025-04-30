79288ca665dc593db789eb3028fa625f5f3b6152
Pranshu Patel <pranshu.patel@simformsolutions.com>
2025-04-30 13:08:23 +0530

    Add(feature/logic): create basic winning logic of game

--- dist/script.js
+++ dist/script.js
@@ -1,17 +1,20 @@
 "use strict";
 class MemoryGame {
     constructor() {
+        this.currentPlayer = 0;
         this.gameArray = new Array(16).fill('');
         this.generateGameArray();
         this.initEventlistner();
+        this.firstCard = null;
+        this.secondCard = null;
+        this.playerScore = [0, 0];
     }
     initEventlistner() {
         var _a;
         (_a = document.querySelector('.gamecontainer')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', (event) => {
             if (event.target.tagName === 'BUTTON') {
                 const btnElement = event.target;
-                btnElement.textContent = btnElement.value;
-                btnElement.disabled = true;
+                this.handleButtonclick(btnElement);
             }
         });
     }
@@ -32,6 +35,36 @@ class MemoryGame {
             const btnElement = document.createElement('button');
             btnElement.value = this.gameArray[i];
             gamecontainer.appendChild(btnElement);
+        }
+    }
+    handleButtonclick(element) {
+        if (this.firstCard == null) {
+            this.firstCard = element;
+            element.textContent = element.value;
+            element.disabled = true;
+        }
+        else if (this.firstCard !== null && this.secondCard === null) {
+            console.log(element);
+            this.secondCard = element;
+            element.textContent = element.value;
+            element.disabled = true;
+            this.handleWinning();
+        }
+    }
+    handleWinning() {
+        if (this.firstCard !== null && this.secondCard !== null) {
+            if (this.firstCard.value === this.secondCard.value) {
+                this.playerScore[this.currentPlayer] = this.playerScore[this.currentPlayer] + 1;
+            }
+            else {
+                this.firstCard.textContent = '';
+                this.firstCard.disabled = false;
+                this.secondCard.textContent = '';
+                this.secondCard.disabled = false;
+                this.currentPlayer = this.currentPlayer === 0 ? 1 : 0;
+            }
+            this.firstCard = null;
+            this.secondCard = null;
         }
     }
 }
--- scripts/script.ts
+++ scripts/script.ts
@@ -1,19 +1,25 @@
 class MemoryGame{
     gameArray: string[];
-    currentPlayer: string;
+    currentPlayer: number;
+    firstCard: HTMLButtonElement|null;
+    secondCard: HTMLButtonElement|null;
+    playerScore: [number,number]
 
     constructor(){
+        this.currentPlayer=0;
         this.gameArray=new Array(16).fill('');
         this.generateGameArray();
         this.initEventlistner();
+        this.firstCard=null;
+        this.secondCard=null;
+        this.playerScore=[0,0];
     }
 
     initEventlistner(){
         document.querySelector('.gamecontainer')?.addEventListener('click',(event: Event)=>{
             if((event.target as HTMLElement).tagName==='BUTTON'){
                 const btnElement=event.target as HTMLButtonElement;
-                btnElement.textContent=btnElement.value;
-                btnElement.disabled=true;
+                this.handleButtonclick(btnElement);
             }
         })
     }
@@ -39,6 +45,40 @@ class MemoryGame{
             const btnElement=document.createElement('button');
             btnElement.value=this.gameArray[i];
             gamecontainer!.appendChild(btnElement);
+        }
+    }
+
+    handleButtonclick(element: HTMLButtonElement){
+        if(this.firstCard==null){
+            this.firstCard=element;
+            element.textContent=element.value;
+            element.disabled=true;
+        }
+        else if(this.firstCard!==null&&this.secondCard===null){
+            console.log(element);
+            
+            this.secondCard=element;
+            element.textContent=element.value;
+            element.disabled=true;
+            this.handleWinning();
+        }
+    }
+
+    handleWinning(){
+        if(this.firstCard!==null&&this.secondCard!==null){
+            if(this.firstCard.value===this.secondCard.value){
+                this.playerScore[this.currentPlayer]=this.playerScore[this.currentPlayer]+1;
+            }
+            else{
+                this.firstCard.textContent='';
+                this.firstCard.disabled=false;
+                this.secondCard.textContent='';
+                this.secondCard.disabled=false;
+                this.currentPlayer=this.currentPlayer===0?1:0;
+            }
+
+            this.firstCard=null;
+            this.secondCard=null;
         }
     }
 }
